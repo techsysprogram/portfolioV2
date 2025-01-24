8e4a810091874cae8cf7e126c2cbc8338a6365e6
Miguel <techsysprogram@gmail.com>
2025-01-24 15:22:23 +0100

    Mise à jour avant déploiement

--- src/components/CardProjet.tsx
+++ src/components/CardProjet.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react"; // ✅ Correction : Import useState
+/* import { useState } from "react"; */ // ✅ Correction : Import useState
 import Button from "./ui/Button";
 import styles from "@/styles/components/ui/Card.module.css"; // ✅ Correction : Utilisation correcte de "styles"
 
-const CardProjet = ({ project }: { project: any }) => {
-  const [open, setOpen] = useState(false);
+interface Project {
+  id: number | string;
+  title: string;
+  summary: string;
+  description: string | string[];
+  link: string;
+  image: string;
+}
+
+
+const CardProjet = ({ project }: { project: Project }) => {
+  /* const [open, setOpen] = useState(false); */
 
   return (
     <div className={styles.card}>
       <div>
         <h3 className={styles.cardTitle}>{project.title}</h3>
         <p className={styles.cardSummary}>{project.summary}</p>
-        <Button onClick={() => setOpen(true)} variant="simple">
+        {/* <Button onClick={() => setOpen(true)} variant="simple"> */}
+        <Button variant="simple">
           Voir plus
         </Button>
       </div>
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import styles from "@/styles/components/ui/Modal.module.css";
 
 interface ModalProps {
