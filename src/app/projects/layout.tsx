fa8144ea7f137dbd4c8e4519c5c59d18e959a7ed
Miguel <techsysprogram@gmail.com>
2025-01-25 14:38:29 +0100

    Fix standalone deployment for Northflank

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,6 +1,6 @@
 export default function ProjectsLayout({ children }: { children: React.ReactNode }) {
     return (
-      <div style={{ maxWidth: "1200px", marginTop: "40px", padding: "20px" }}>
+      <div style={{ marginTop: "50px", padding: "20px" }}>
         {children}
       </div>
     );
