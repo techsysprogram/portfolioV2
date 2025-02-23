8d489fa0063494caa788308fc1b6a910e98acede
Miguel <techsysprogram@gmail.com>
2025-02-23 09:39:42 +0100

    chore: mise à jour générale

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,19 +4,37 @@ import styles from "@/styles/components/Hero.module.css";
 import "@/styles/text-styles.css";
 
 const floatingIcons = [
-  { id: 1, name: "Next.js", image: "https://res.cloudinary.com/dshznc4xx/image/upload/v1737735139/next-js_kzcyc2.svg", style: styles.iconNext },
-  { id: 2, name: "React.js", image: "https://res.cloudinary.com/dshznc4xx/image/upload/v1737735139/react-2_r1cjjd.svg", style: styles.iconReact },
+  {
+    id: 1,
+    name: "Next.js",
+    image:
+      "https://res.cloudinary.com/dshznc4xx/image/upload/v1737735139/next-js_kzcyc2.svg",
+    style: styles.iconNext,
+  },
+  {
+    id: 2,
+    name: "React.js",
+    image:
+      "https://res.cloudinary.com/dshznc4xx/image/upload/v1737735139/react-2_r1cjjd.svg",
+    style: styles.iconReact,
+  },
 ];
 
 export default function Hero() {
-  const [profileImage, setProfileImage] = useState("https://res.cloudinary.com/dshznc4xx/image/upload/a_hflip/v1738228843/MiguelPC_mfdchi.jpg");
+  const [profileImage, setProfileImage] = useState(
+    "https://res.cloudinary.com/dshznc4xx/image/upload/a_hflip/v1738228843/MiguelPC_mfdchi.jpg"
+  );
 
   useEffect(() => {
     const updateImage = () => {
       if (window.innerWidth <= 768) {
-        setProfileImage("https://res.cloudinary.com/dshznc4xx/image/upload/v1737478718/MiguelPC_d1f2ux.jpg");
+        setProfileImage(
+          "https://res.cloudinary.com/dshznc4xx/image/upload/v1737478718/MiguelPC_d1f2ux.jpg"
+        );
       } else {
-        setProfileImage("https://res.cloudinary.com/dshznc4xx/image/upload/a_hflip/v1738228843/MiguelPC_mfdchi.jpg");
+        setProfileImage(
+          "https://res.cloudinary.com/dshznc4xx/image/upload/a_hflip/v1738228843/MiguelPC_mfdchi.jpg"
+        );
       }
     };
 
@@ -28,16 +46,32 @@ export default function Hero() {
   return (
     <header className={styles.hero}>
       {floatingIcons.map((icon) => (
-        <Image key={icon.id} src={icon.image} alt={icon.name} width={70} height={70} className={`${styles.floatingIcon} ${icon.style}`} />
+        <Image
+          key={icon.id}
+          src={icon.image}
+          alt={icon.name}
+          width={70}
+          height={70}
+          className={`${styles.floatingIcon} ${icon.style}`}
+        />
       ))}
 
       <div className={styles.container}>
-        <Image src={profileImage} alt="Miguel Bellota" width={200} height={200} className={styles.image} />
+        <Image
+          src={profileImage}
+          alt="Miguel Bellota"
+          width={200}
+          height={200}
+          priority
+          className={styles.image}
+        />
 
         <div className={styles.textContainer}>
           <h1 className="title color-white">Miguel Bellota</h1>
           <p className="text color-white">
-            Développeur Logiciel & Full Stack | Passionné par les technologies, je conçois des applications intuitives et agréables à utiliser pour offrir une expérience utilisateur optimale.
+            Développeur Logiciel & Full Stack | Passionné par les technologies,
+            je conçois des applications intuitives et agréables à utiliser pour
+            offrir une expérience utilisateur optimale.
           </p>
         </div>
       </div>
